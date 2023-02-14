e0a95c56be0bb01f5c238317ccdb4e39e04f2433
mariopoons <mariopons02@gmail.com>
2023-02-14 12:33:32 +0100

    hecho

--- Assets/Scripts/Exercise5.cs
+++ Assets/Scripts/Exercise5.cs
@@ -4,5 +4,19 @@ using UnityEngine;
 
 public class Exercise5 : MonoBehaviour
 {
+    public Vector3[] positions;
 
+    private void Start()
+    {
+        StartCoroutine (MoveCube());
+    }
+
+    private IEnumerator MoveCube()
+    {
+        foreach(Vector3 pos in positions) // recorrerá el array que digamos, en este caso, el array Vector3
+        {
+            transform.position = pos;
+            yield return new WaitForSeconds(1f);
+        }
+    }
 }
